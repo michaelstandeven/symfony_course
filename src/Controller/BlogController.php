1afa4c9c5de3685778f8c90d87a746fb84124c0a
mstandeven <mstandeven@xpressdocs.com>
2018-07-26 16:13:38 -0500

    17.1 About routing 1:51
    17.1 About routing 1:51
    Replace: http://www.symfony.course.com/Robert
    With http://www.symfony.course.com/blog/Robert

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -17,6 +17,9 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/blog")
+ */
 class BlogController
 {
     /**
