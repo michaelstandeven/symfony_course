72884bd8141396b14ee55b34ada69bd83ded7a83
mstandeven <mstandeven@xpressdocs.com>
2018-07-27 09:36:21 -0500

    18.3 Introduction to views (Twig) 9:25-9:38
    18.3 Introduction to views (Twig) 9:25-9:38
    Working: http://www.symfony.course.com/blog/

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -11,12 +11,15 @@ namespace App\Controller;
 use App\Service\VeryBadDesign;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\httpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\RouterInterface;
+
+
 
 /**
  * @Route("/blog")
