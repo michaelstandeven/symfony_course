b0dac51abfa86e6686e1d339af2b87c6a8f11d58
mstandeven <mstandeven@xpressdocs.com>
2018-07-27 14:12:31 -0500

    20. Custom error pages using Twig
    Added custom error pages
    20. Custom error pages using Twig

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -15,7 +15,7 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
-use Symfony\Component\httpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\RouterInterface;
 
