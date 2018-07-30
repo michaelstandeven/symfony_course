a1dbaa18cbd49fdc6e1d174969aecb57c8429659
michaelstandeven <mstandeven@xpressdocs.com>
2018-07-30 16:33:31 -0500

    31. Repository findBy, form validation, handling Entity changes
    Edit form

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -57,11 +57,38 @@ class MicroPostController
         $html = $this->twig->render(
             'micro-post/index.html.twig',
             [
-                'posts' => $this->microPostRepository->findAll()
+                'posts' => $this->microPostRepository->findBy([], ['time' => 'DESC']),
             ]
         );
 
         return new Response($html);
+    }
+
+    /**
+     * @Route("/edit/{id}", name="micro_post_edit")
+     */
+    public function edit(MicroPost $microPost, Request $request)
+    {
+        $form = $this->formFactory->create(
+            MicroPostType::class,
+            $microPost
+        );
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->entityManager->persist($microPost);
+            $this->entityManager->flush();
+
+            return new RedirectResponse(
+                $this->router->generate('micro_post_index')
+            );
+        }
+        return new Response(
+            $this->twig->render(
+                'micro-post/add.html.twig',
+                ['form' => $form->createView()]
+            )
+        );
     }
 
     /**
