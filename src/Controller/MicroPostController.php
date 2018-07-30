1e463a8d8d4acf8f34391e7c00f343300803edc4
michaelstandeven <mstandeven@xpressdocs.com>
2018-07-30 17:06:00 -0500

    32.2 Deleting an Entity and flash messages
    Added a delete button
    https://www.udemy.com/learn-symfony-4-hands-on-creating-a-real-world-application/learn/v4/t/lecture/10197946?start=0

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -9,6 +9,7 @@ use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\RouterInterface;
 
@@ -37,17 +38,23 @@ class MicroPostController
      * @var RouterInterface
      */
     private $router;
+    /**
+     * @var FlashBagInterface
+     */
+    private $flashBag;
 
     public function __construct(
         \Twig_Environment $twig, MicroPostRepository $microPostRepository,
         FormFactoryInterface $formFactory, EntityManagerInterface $entityManager,
-        RouterInterface $router
+        RouterInterface $router,
+        FlashBagInterface $flashBag
     ){
         $this->twig = $twig;
         $this->microPostRepository = $microPostRepository;
         $this->formFactory = $formFactory;
         $this->entityManager = $entityManager;
         $this->router = $router;
+        $this->flashBag = $flashBag;
     }
     /**
      * @Route("/", name="micro_post_index")
@@ -88,6 +95,19 @@ class MicroPostController
                 'micro-post/add.html.twig',
                 ['form' => $form->createView()]
             )
+        );
+    }
+
+    /**
+     * @Route("/delete/{id}", name="micro_post_delete")
+     */
+        $this->entityManager->remove($microPost);
+        $this->entityManager->flush();
+
+        $this->flashBag->add('notice', 'Micro post was deleted');
+
+        return new RedirectResponse(
+            $this->router->generate('micro_post_index')
         );
     }
 
