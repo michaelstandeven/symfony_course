7e699f7ee4659adc2cacdc999e043d181f1f971e
michaelstandeven <mstandeven@xpressdocs.com>
2018-07-30 16:22:55 -0500

    30. Repository find methods and Twig include
    Added links
    http://www.symfony.course.com/micro-post/

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -93,4 +93,19 @@ class MicroPostController
             )
         );
     }
+
+    /**
+     * @Route("/{id}", name="micro_post_post")
+     */
+    public function post(MicroPost $post)
+    {
+        return new Response(
+            $this->twig->render(
+                'micro-post/post.html.twig',
+                [
+                    'post' => $post
+                ]
+            )
+        );
+    }
 }
