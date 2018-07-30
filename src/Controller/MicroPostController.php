02546b4bc4f11672c946b59a651738c92eb9585c
michaelstandeven <mstandeven@xpressdocs.com>
2018-07-30 15:54:05 -0500

    29.4 Creating a form and handling form submission
    Can add blog posts
    http://www.symfony.course.com/micro-post/

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -4,10 +4,13 @@ namespace App\Controller;
 use App\Entity\MicroPost;
 use App\Form\MicroPostType;
 use App\Repository\MicroPostRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\RouterInterface;
 
 /**
  * @Route("/micro-post")
@@ -26,14 +29,25 @@ class MicroPostController
      * @var FormFactoryInterface
      */
     private $formFactory;
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+    /**
+     * @var RouterInterface
+     */
+    private $router;
 
     public function __construct(
         \Twig_Environment $twig, MicroPostRepository $microPostRepository,
-        FormFactoryInterface $formFactory
+        FormFactoryInterface $formFactory, EntityManagerInterface $entityManager,
+        RouterInterface $router
     ){
         $this->twig = $twig;
         $this->microPostRepository = $microPostRepository;
         $this->formFactory = $formFactory;
+        $this->entityManager = $entityManager;
+        $this->router = $router;
     }
     /**
      * @Route("/", name="micro_post_index")
@@ -65,7 +79,12 @@ class MicroPostController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $this->entityManager->persist($microPost);
+            $this->entityManager->flush();
 
+            return new RedirectResponse(
+                $this->router->generate('micro_post_index')
+            );
         }
         return new Response(
             $this->twig->render(
