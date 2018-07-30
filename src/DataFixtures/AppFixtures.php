4f5c909b89e25a93304b5bb0c543b792a90d8b4d
michaelstandeven <mstandeven@xpressdocs.com>
2018-07-30 15:02:02 -0500

    28.2 Creating fixtures (database seeding)
    Added use statements
    http://www.symfony.course.com/micro-post

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -9,19 +9,21 @@
 namespace App\DataFixtures;
 
 
+use App\Entity\MicroPost;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        for ($i = 0; $i<10;$i++){
+        for ($i = 0; $i < 10;$i++){
             $microPost = new MicroPost();
             $microPost->setText('Some random text' . rand(0, 100));
             $microPost->setTime(new \DateTime('2018-03-15'));
             $manager->persist($microPost);
         }
 
-        $manager->flush;
+        $manager->flush();
     }
 }
