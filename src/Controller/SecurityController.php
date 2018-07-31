84bf76452fea337bd91a27c76798c28220617610
michaelstandeven <mstandeven@xpressdocs.com>
2018-07-31 09:22:01 -0500

    39. Login form part 2
    Login and logout.
    http://www.symfony.course.com/login/

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -8,23 +8,31 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController
 {
+    /**
+     * @var \Twig_Environment
+     */
+    private $twig;
 
     public function __construct(\Twig_Environment $twig)
     {
         $this->twig = $twig;
     }
+
     /**
      * @Route("/login", name="security_login")
      */
     public function login(AuthenticationUtils $authenticationUtils)
     {
-        return new Response($this->twig->render(
-            'security/login.html.twig',
-            [
-                'last_username' => $authenticationUtils->getLastUsername(),
-                'error' => $authenticationUtils->getLastAuthenticationError()
-            ]
-        ));
+        return new Response(
+            $this->twig->render(
+                'security/login.html.twig',
+                [
+                    'last_username' => $authenticationUtils->getLastUsername(),
+                    'error' => $authenticationUtils->getLastAuthenticationError(
+                    ),
+                ]
+            )
+        );
     }
 
     /**
