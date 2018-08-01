df96d2d8ae1eee6427d71a9b7ef126aa9a4a0b69
michaelstandeven <mstandeven@xpressdocs.com>
2018-07-31 19:58:51 -0500

    52. Restricting adding new posts to registered users (many ways of doing that)
    Micro post addedhttp://www.symfony.course.com/micro-post/add

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -15,6 +15,7 @@ use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
 /**
@@ -131,11 +132,15 @@ class MicroPostController
 
     /**
      * @Route("/add", name="micro_post_add")
+     * @Security("is_granted('ROLE_USER')")
      */
-    public function add(Request $request)
+    public function add(Request $request, TokenStorageInterface $tokenStorage)
     {
+        $user = $tokenStorage->getToken()->getUser();
+
         $microPost = new MicroPost();
         $microPost->setTime(new \DateTime());
+        $microPost->setUser($user);
 
         $form = $this->formFactory->create(
             MicroPostType::class,
