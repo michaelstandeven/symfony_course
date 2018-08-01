06a90268fa11bc04ce4b6f212e1abf7ca5dbf82d
michaelstandeven <mstandeven@xpressdocs.com>
2018-08-01 10:08:31 -0500

    55. ManyToMany self-referencing relation (following/followers)
    Adding followers and followed to database.

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -67,9 +67,29 @@ class User implements UserInterface, \Serializable
      */
     private $posts;
 
+    /**
+     * @ORM\ManyToMany(targetEntity="App\Entity\User", mappedBy="following")
+     */
+    private $followers;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="App\Entity\User", inversedBy="followers")
+     * @ORM\JoinTable(name="following",
+     *      joinColumns={
+     *          @ORM\JoinColumn(name="user_id", referencedColumnName="id")
+     *      },
+     *      inverseJoinColumns={
+     *          @ORM\JoinColumn(name="following_user_id", referencedColumnName="id")
+     *      }
+     * )
+     */
+    private $following;
+
     public function __construct()
     {
         $this->posts = new ArrayCollection();
+        $this->followers = new ArrayCollection();
+        $this->following = new ArrayCollection();
     }
 
     public function getRoles()
@@ -203,5 +223,22 @@ class User implements UserInterface, \Serializable
     {
         return $this->posts;
     }
+
+    /**
+     * @return mixed
+     */
+    public function getFollowers()
+    {
+        return $this->followers;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFollowing()
+    {
+        return $this->following;
+    }
+
 
 }
