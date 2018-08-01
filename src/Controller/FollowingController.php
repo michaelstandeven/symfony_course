761b394ebdd5e4b390ee63ca9485bf8376aeb3ea
michaelstandeven <mstandeven@xpressdocs.com>
2018-08-01 15:33:43 -0500

    62. More on Doctrine Collections (from the database perspective)
    http://www.symfony.course.com/micro-post/user/john_doe

--- src/Controller/FollowingController.php
+++ src/Controller/FollowingController.php
@@ -22,8 +22,7 @@ class FollowingController extends Controller
         $currentUser = $this->getUser();
 
         if ($userToFollow->getId() !== $currentUser->getId()) {
-            $currentUser->getFollowing()
-                ->add($userToFollow);
+            $currentUser->follow($userToFollow);
 
             $this->getDoctrine()
                 ->getManager()
