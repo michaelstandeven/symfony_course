d3dfd0559eddd00ea99721e8b1bb278932c41e6f
michaelstandeven <mstandeven@xpressdocs.com>
2018-08-02 17:05:33 -0500

    78. Sending e-mail after user registers using Swift Mailer
    Sending emails and viewing on profiler
    http://www.symfony.course.com/_profiler/

--- src/Event/UserRegisterEvent.php
+++ src/Event/UserRegisterEvent.php
@@ -1,26 +1,19 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: mstandeven
- * Date: 8/2/2018
- * Time: 2:11 PM
- */
 
 namespace App\Event;
 
+use App\Entity\User;
 use Symfony\Component\EventDispatcher\Event;
 
 class UserRegisterEvent extends Event
 {
     const NAME = 'user.register';
+
     /**
      * @var User
      */
     private $registeredUser;
 
-    /**
-     * UserRegisterEvent constructor.
-     */
     public function __construct(User $registeredUser)
     {
         $this->registeredUser = $registeredUser;
--- src/Event/UserSubscriber.php
+++ src/Event/UserSubscriber.php
@@ -2,10 +2,28 @@
 
 namespace App\Event;
 
+use App\Entity\UserPreferences;
+use App\Mailer\Mailer;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class UserSubscriber implements EventSubscriberInterface
 {
+    /**
+     * @var \Swift_Mailer
+     */
+    private $mailer;
+    /**
+     * @var \Twig_Environment
+     */
+    private $twig;
+
+    public function __construct(\Swift_Mailer $mailer, \Twig_Environment $twig)
+    {
+        $this->mailer = $mailer;
+        $this->twig = $twig;
+    }
+
     public static function getSubscribedEvents()
     {
         return [
@@ -15,6 +33,16 @@ class UserSubscriber implements EventSubscriberInterface
 
     public function onUserRegister(UserRegisterEvent $event)
     {
+        $body = $this->twig->render('email/registration.html.twig', [
+            'user' => $event->getRegisteredUser()
+        ]);
 
+        $message = (new \Swift_Message())
+            ->setSubject('Welcome to the micro-post app!')
+            ->setFrom('micropost@micropost.com')
+            ->setTo($event->getRegisteredUser()->getEmail())
+            ->setBody($body, 'text/html');
+
+        $this->mailer->send($message);
     }
 }
